fcf3ee6281d77446e1a4c31d9778d67127ea36e5
Vladislav <kulik24091983@gmail.com>
2021-02-12 18:05:56 +0200

    Task 2

--- PZ_1.11/perdushnya-Yakas.js
+++ PZ_1.11/perdushnya-Yakas.js
@@ -1,4 +1,10 @@
 function seconds(total) {
     return total % 60
-}
-console.log(seconds(150))
+}console.log(seconds(150))
+
+//      Task 2
+function perimeter(side, count) {
+    return side * count
+}console.log(perimeter(4,3))
+
+
