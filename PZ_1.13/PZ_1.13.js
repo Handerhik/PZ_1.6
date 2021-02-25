84c12b30fd8c009887ca424f8e949a11f4b4e1eb
Vladislav <kulik24091983@gmail.com>
2021-02-25 18:23:47 +0200

    Task 5

--- PZ_1.13/PZ_1.13.js
+++ PZ_1.13/PZ_1.13.js
@@ -120,3 +120,17 @@
 // console.log(Random.nextDouble(2.3, 14.7))
 // console.log(Random.nextInt(2, 10))
 // console.log(Random.nextElement([1, 4, 5, 8, 25]));
+
+//      TASK 5
+//
+// function sequence(start, step) {
+//     return () => (start === 0 ? start += 1 : start += step)
+// }
+// let generator = sequence(10, 3);
+// let generator2 = sequence(7,1);
+//
+// console.log(generator());
+// console.log(generator2());
+// console.log(generator());
+// console.log(generator2());
+// console.log(generator());
