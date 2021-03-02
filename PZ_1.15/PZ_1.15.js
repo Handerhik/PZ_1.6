f90349b76e455ebcf6df995c2f24ee0606dab555
Vladislav <kulik24091983@gmail.com>
2021-03-02 10:51:43 +0200

    PZ_1.15 TASK 1

---
+++ PZ_1.15/PZ_1.15.js
@@ -0,0 +1,20 @@
+
+//      TASK 1
+// let obj = {}
+// function numberToObj(number) {
+//     if (number > 9999 || number < 1){
+//         return ('Number can not be higher than 9999 or lower than 1' + '\n' + obj)
+//     }
+//     else{
+//         let arr = number.toString().split('').reverse()
+//         return{
+//             units: !arr[0] ? 0 : arr[0],
+//             tens: !arr[1] ? 0 : arr[1],
+//             hundreds: !arr[2] ? 0 : arr[2],
+//             thousands: !arr[3] ? 0 : arr[3]
+//         }
+//     }
+//
+// }
+// console.log(numberToObj(6754))
+
