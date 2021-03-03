b22198869262d80a54bf8ae97040d77ce4027575
Vladislav <kulik24091983@gmail.com>
2021-03-03 15:25:46 +0200

    PZ_2.3 TASK 2

--- PZ_2.3/script.js
+++ PZ_2.3/script.js
@@ -1,3 +1,10 @@
 //      TASK 1
 // const os = require("os");
 // console.log(os.userInfo().username);
+
+//      TASK 2
+const colors = require('colors')
+const say = require('say')
+console.log(colors.random('Hello'))
+//say.play('C:\\Users\\Vladislav\\WebstormProjects\\PZ_1.4\\PZ_2.3')
+say.speak('sound-play for some reason does not work, so I did it using the say module')
