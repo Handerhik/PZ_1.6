550af7e6e69980d2d47fe0727954f4a2a938abfc
Vladislav <kulik24091983@gmail.com>
2021-03-03 17:40:51 +0200

    PZ_2.3 TASK 3

--- PZ_2.3/script.js
+++ PZ_2.3/script.js
@@ -3,8 +3,21 @@
 // console.log(os.userInfo().username);
 
 //      TASK 2
-const colors = require('colors')
-const say = require('say')
-console.log(colors.random('Hello'))
-//say.play('C:\\Users\\Vladislav\\WebstormProjects\\PZ_1.4\\PZ_2.3')
-say.speak('sound-play for some reason does not work, so I did it using the say module')
+// const colors = require('colors')
+// const say = require('say')
+// console.log(colors.random('Hello'))
+// //say.play('C:\\Users\\Vladislav\\WebstormProjects\\PZ_1.4\\PZ_2.3')
+// say.speak('sound-play for some reason does not work, so I did it using the say module')
+
+//      TASK 3
+// const path = require('path')
+// let way = 'C:\\Users\\Vladislav\\WebstormProjects\\PZ_1.4\\PZ_2.3\\index.html'
+// console.log(path.basename(way))
+// console.log(path.extname(way))
+// if (new RegExp(/^[A-Z]?:/i).test(way)){
+//     console.log('Microsoft Windows')
+// }
+// else if (new RegExp(/^[/]?[a-z]/i).test(way)){
+//     console.log('Unixxxx')
+// }
+// else console.log('error')
